c072f3469cfcaa7c22495f07b61fcab4894b0304
Matthieu <matthieu258@hotmail.com>
2019-04-28 15:05:53 +0200

    remove useless calls

--- www/js/services.js
+++ www/js/services.js
@@ -57,7 +57,6 @@ $cordovaSQLite.execute(db, 'DELETE FROM T_SPORT WHERE id=6');
           .then(function(res){
           }, onErrorQuery)
 
-        initTrainingPlanDB();
 
       //  $cordovaSQLite.execute(db, 'DROP TABLE T_TRAINING');
       $cordovaSQLite.execute(db, 'CREATE TABLE IF NOT EXISTS T_TRAINING (id integer primary key, sport_id, duration, distance, trainingDate date, imgUrl, title, content)')
